528972edb478f6c906cf8c11b08fa3feebf8c0ea
Marcos Martinelli <marcos@MarcMini.local>
2024-01-31 06:12:46 -0800

    variables; update main; remove sensitive files/gitignore

--- module_1_basics_n_setup/terraform-basics/main.tf
+++ module_1_basics_n_setup/terraform-basics/main.tf
@@ -8,24 +8,25 @@ terraform {
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "terraform-de-camp"
-  region  = "us-west2-a"
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  # if creating a variable for credentials, you can use a function
+  #  credentials = file(var.credentials)
+  project = var.gcs_project_name
+  region  = var.region
 }
 
 
 
 resource "google_storage_bucket" "terraform-basics-bucket" {
-  name          = "terraform-de-camp-butket"
-  location      = "US"
+  name     = var.gcs_bucket_name
+  location = var.location
 
   # Optional, but recommended settings:
-  storage_class = "STANDARD"
+  storage_class               = var.gcs_storage_class
   uniform_bucket_level_access = true
 
   versioning {
-    enabled     = true
+    enabled = true
   }
 
   lifecycle_rule {
@@ -33,7 +34,7 @@ resource "google_storage_bucket" "terraform-basics-bucket" {
       type = "Delete"
     }
     condition {
-      age = 30  // days
+      age = 30 // days
     }
   }
 
@@ -42,7 +43,7 @@ resource "google_storage_bucket" "terraform-basics-bucket" {
 
 
 resource "google_bigquery_dataset" "terraform-basics-dataset" {
-  dataset_id = "terraform_de_camp_dataset"
-  project    = "terraform-de-camp"
-  location   = "US"
+  dataset_id = var.bq_dataset_name
+  project    = var.gcs_project_name
+  location   = var.location
 }
---
+++ module_1_basics_n_setup/terraform-basics/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+    description = "GCS Credentials location"
+    default = "./keys/terraform-de-camp-cred.json"
+}
+
+variable "gcs_project_name" {
+  description = "My GCS Project name"
+  default     = "terraform-de-camp"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "terraform_de_camp_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket name"
+  default     = "terraform-de-camp-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "My Storage Bucket Class"
+  default     = "STANDARD"
+}
+
+variable "location" {
+  description = "Project location"
+  default     = "US"
+}
+
+variable "region" {
+  description = "Project region setting"
+  default     = "us-west2-a"
+}
